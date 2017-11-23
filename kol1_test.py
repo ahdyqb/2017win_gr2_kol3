ae52e3d81ccde765079efc5e2f9ddcff023037fa
Jakub Ahaddad <j.ahaddad@protonmail.com>
2017-11-23 17:36:08 +0100

    corrected

--- kol1_test.py
+++ kol1_test.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python2
+
+#original code author: github.com/Korfeusz
+
 import unittest
 from kol1_korfeusz import Environment, Regulator
-#import numpy as np
+import math
+from __future__ import division
 
 class test_korfeusz(unittest.TestCase):
 	def setUp(self):
 		self.environment = Environment(4.7,60)
 		self.regulator = Regulator(environment=self.environment, \
 			correction_rate=0.5, correct_value=0.0)
+			
+	def test_check_if_correct_angle_conversion(self):
+		checked_sine = math.sin(self.environment.wind_direction*math.pi/180.0)
+		self.environment.calculate_wind_direction_input_multiplier()
+		self.assertAlmostEqual(checked_sine, self.environment.random_wind_impact, places=4)
 	
 	def test_check_if_tilt_corrected(self):
 		current_tilt = 0.47
